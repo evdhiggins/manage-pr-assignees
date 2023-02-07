dd69afe87c6078d73e134b21763d6acd0e44246f
Evan Higgins <evdhiggins@gmail.com>
2023-02-06 20:40:23 -0600

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -12,24 +12,26 @@ const errors_1 = __nccwpck_require__(9062);
 function determineAssigneesForPrAndThrowIfNoCreator({ pr, event, creatorAssigneeSubstitutions, }) {
     if (!pr.user)
         throw new errors_1.NoPullRequestCreatorFoundError(pr.number);
+    const effectivePrCreator = pr.user.login in creatorAssigneeSubstitutions
+        ? creatorAssigneeSubstitutions[pr.user.login]
+        : pr.user.login;
     const currentlyAssignedUsers = pr.assignees?.map(pluckLogin) ?? [];
     const usersWithPendingReviewRequests = pr.requested_reviewers?.map(pluckLogin) ?? [];
+    const isReviewRequestedEvent = event === 'review-requested';
     /** Return true if a given user login should be unassigned */
-    const shouldNotBeAssigned = (login) => !usersWithPendingReviewRequests.includes(login);
+    const shouldNotBeAssigned = (login) => !usersWithPendingReviewRequests.includes(login) &&
+        // Only automatically un-assign PR creator when a review is requested
+        (isReviewRequestedEvent || login !== effectivePrCreator);
     /** Return true if a given user login should be assigned (and is not currently) */
     const shouldBeAssigned = (login) => !currentlyAssignedUsers.includes(login);
     const toUnassign = currentlyAssignedUsers.filter(shouldNotBeAssigned);
     const toAssign = usersWithPendingReviewRequests.filter(shouldBeAssigned);
     const isReviewSubmittedNotApprovedEvent = event === 'review-submitted-not-approved';
     const noOutstandingReviewRequestsRemain = usersWithPendingReviewRequests.length === 0;
-    if (isReviewSubmittedNotApprovedEvent || noOutstandingReviewRequestsRemain) {
-        if (pr.user.login in creatorAssigneeSubstitutions) {
-            const creatorSubstituteAssignee = creatorAssigneeSubstitutions[pr.user.login];
-            toAssign.push(creatorSubstituteAssignee);
-        }
-        else {
-            toAssign.push(pr.user.login);
-        }
+    const prCreatorIsAlreadyAssigned = currentlyAssignedUsers.includes(effectivePrCreator);
+    if ((isReviewSubmittedNotApprovedEvent || noOutstandingReviewRequestsRemain) &&
+        !prCreatorIsAlreadyAssigned) {
+        toAssign.push(effectivePrCreator);
     }
     return { toAssign, toUnassign };
 }
